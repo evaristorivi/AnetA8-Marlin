ca2291e0bcc0e62bf480309ff87c66a2e0d09dea
Evaristo R <evaristorivieccio@gmail.com>
2026-05-26 23:10:29 +0200

    Ajustar offset reales y solucionar max de eje y que golpeaba por delante

--- Anet 8 con SKR 1.4 Turbo y 3dTouch/Configuration.h
+++ Anet 8 con SKR 1.4 Turbo y 3dTouch/Configuration.h
@@ -1516,7 +1516,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -18, -45, -0.60 }
+#define NOZZLE_TO_PROBE_OFFSET { -18, -45, -0.70 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1734,7 +1734,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -33
-#define Y_MIN_POS -10
+#define Y_MIN_POS -3
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
